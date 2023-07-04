9186ba16155ad2a975cf3556a57557ebf400c082
javo2023 <jguzmanalarcon@gmail.com>
2023-07-03 22:44:56 -0400

    commit final

---
+++ app/src/main/java/com/example/ejercicio_1_m5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ejercicio_1_m5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
